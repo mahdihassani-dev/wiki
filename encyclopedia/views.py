99a779656c6fd7c3a7ac97c3dd5b9aad029197e2
Mahdi Hassani <m.hassani4951383@gmail.com>
2024-11-30 19:23:14 +0330

    search feature completed

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render, HttpResponse
-from django.http import HttpResponseNotFound
+from django.shortcuts import render
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 import markdown
 
 
 def index(request):
-    
     search = request.GET.get('q', '')
     head = 'All Pages' if search == '' else f'Search Result for "{search}"'
+    
+    entries = util.list_entries(search)
+    for entry in entries:
+        if search.lower() == entry.lower():
+            return HttpResponseRedirect(reverse('entry', args=[search])) # redirect to entry of that content
 
     return render(request, "encyclopedia/index.html", {"entries": util.list_entries(search), "head": head})
 
@@ -18,3 +23,5 @@ def entry(request, title):
         # convert markdown -> html
         return render(request, "encyclopedia/entry.html", {"title": title, "content": markdown.markdown(util.get_entry(title))})
     return HttpResponseNotFound(f"There is no content for {title}")
+
+
