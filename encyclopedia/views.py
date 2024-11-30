00159114a20ebb39f546fd22df61e6fe95c28ea4
Mahdi Hassani <m.hassani4951383@gmail.com>
2024-11-30 16:34:01 +0330

    entry page feature added

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseNotFound
 
 from . import util
+import markdown
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
+
+def entry(request, title):
+    if util.get_entry(title):
+        # convert markdown -> html
+        return render(request, "encyclopedia/entry.html", {"title": title, "content": markdown.markdown(util.get_entry(title))})
+    return HttpResponseNotFound(f"There is no content for {title}")
